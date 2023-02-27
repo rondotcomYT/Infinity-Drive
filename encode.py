4cb6afe425e411caeeda840ab049414643b99230
rondotcomYT <124475503+rondotcomYT@users.noreply.github.com>
2023-02-27 18:46:14 -0500

    Update and rename encode V2.py to encode.py

--- encode.py
+++ encode.py
@@ -1,3 +1,4 @@
+# import libraries
 import PIL
 import cv2
 from PIL import Image, ImageDraw
@@ -8,55 +9,67 @@ import os
 # black = 0
 # white = 1
 
+# deletes files from /temp/ folder
 for root, dirs, files in os.walk('./temp/'):
     for f in files:
         os.unlink(os.path.join(root, f))
     for d in dirs:
         shutil.rmtree(os.path.join(root, d))
 
-file = "Comp 1.mp4"
+file = input("What file should I encode?: ")
+file_size = os.path.getsize(file)
 width = 640
 height = 480
 w_pix = 0
 h_pix = 0
-frame = 1
+frame = 0
 img = PIL.Image.new('1', (width, height), "black")
 d = ImageDraw.Draw(img)
 
+if int(round(int(file_size) / int(38400))) < 1:
+    print("Generating 1 image...")
+elif int(round(int(file_size) / int(38400))) >= 1:
+    print("Generating " + str(round(int(file_size) / int(38400))) + " images...")
+
 with open(file, "rb") as f:
     while byte := f.read(1):
-        byte = ' '.join(format(ord(x), 'b') for x in str(byte)[2:-1]).replace(" ", "")
-        for i in range(len(byte)):
-            if byte[i] == "1":
+        binary = "{0:08b}".format(int(hex(byte[0])[2:], 16))
+        for a in range(len(binary)):
+            if binary[a] == "1":
                 d.point((w_pix, h_pix), fill="white")
             w_pix += 1
             if w_pix > width - 1:
-                h_pix += 1
                 w_pix = 0
+                h_pix += 1
                 if h_pix > height - 1:
-                    h_pix = 0
                     w_pix = 0
+                    h_pix = 0
                     img.save("./temp/" + str(frame) + ".png")
                     img = PIL.Image.new('RGB', (width, height), "black")
                     d = ImageDraw.Draw(img)
                     frame += 1
-
 img.save("./temp/" + str(frame) + ".png")
+frame += 1
 
 image_folder = './temp/'
-video_name = 'archive.avi'
+video_name = 'infinity_drive.avi'
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+img_frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = img_frame.shape
 
 video = cv2.VideoWriter(video_name, 0, 30, (width, height))
 
-# fix this, inefficient
 i = 0
-for image in tqdm(images):
-    i += 1
+for image in tqdm(range(frame), unit=' FP'):
     video.write(cv2.imread(os.path.join(image_folder, str(i) + ".png")))
+    i += 1
+
+for root, dirs, files in os.walk('./temp/'):
+    for f in files:
+        os.unlink(os.path.join(root, f))
+    for d in dirs:
+        shutil.rmtree(os.path.join(root, d))
 
 video.release()
 cv2.destroyAllWindows()
