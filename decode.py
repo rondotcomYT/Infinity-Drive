d886e780d8c9f3a9d221f54a02343de186c5aa90
rondotcomYT <124475503+rondotcomYT@users.noreply.github.com>
2023-02-27 18:45:35 -0500

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,13 +1,11 @@
-import PIL
-from tqdm import tqdm
-from PIL import Image, ImageDraw
-import cv2
-import shutil
 import os
+import shutil
+import cv2
+from PIL import Image
+from tqdm import tqdm
 
-cam = cv2.VideoCapture("archive.avi")
-currentframe = 0
-process_frame = 1
+cam = cv2.VideoCapture(input("What file should I decode?: "))
+frames = 0
 
 for root, dirs, files in os.walk('./temp/'):
     for f in files:
@@ -18,29 +16,43 @@ for root, dirs, files in os.walk('./temp/'):
 while True:
     ret, frame = cam.read()
     if ret:
-        name = "./temp/" + str(currentframe) + ".png"
+        name = "./temp/" + str(frames) + ".png"
         cv2.imwrite(name, frame)
-        currentframe += 1
+        frames += 1
     else:
         break
 
-width = cv2.imread("./temp/1.png", cv2.IMREAD_UNCHANGED).shape[1]
-height = cv2.imread("./temp/1.png", cv2.IMREAD_UNCHANGED).shape[0]
-print(width)
-print(height)
-
+width = cv2.imread("./temp/0.png", cv2.IMREAD_UNCHANGED).shape[1]
+height = cv2.imread("./temp/0.png", cv2.IMREAD_UNCHANGED).shape[0]
 
 x = 0
 y = 0
 coordinate = x, y
-frame = 1
-image = Image.open(r"./temp/" + str(frame) + ".png")
-while currentframe > process_frame:
-    binary = ""
-    for i in range(7):
-        color = image.getpixel(coordinate)
-        print(color)
-        if color[0] > 128:
-            binary = binary + "1"
-        elif color[0] < 128:
-            binary = binary + "0"
+binary = ""
+frame = 0
+
+with open(input("What should I name the output file? (i.e output.zip): "), "wb") as file:
+    for a in tqdm(range(frames), unit=' FP'):
+        image = Image.open("./temp/" + str(frame) + ".png")
+        for b in range(480):
+            for c in range(80):
+                for i in range(8):
+                    coordinate = c * int(8) + i, b
+                    color = image.getpixel(coordinate)
+                    if color[0] > 128:
+                        binary = binary + "1"
+                    elif color[0] < 128:
+                        binary = binary + "0"
+                if len(hex(int(binary, 2))) > 3:
+                    file.write(bytearray.fromhex(hex(int(binary, 2))[2:]))
+                elif len(hex(int(binary, 2))) == 3:
+                    file.write(bytearray.fromhex(hex(int(binary, 2)).replace("0x", "0")))
+                binary = ""
+        frame += 1
+file.close()
+
+for root, dirs, files in os.walk('./temp/'):
+    for f in files:
+        os.unlink(os.path.join(root, f))
+    for d in dirs:
+        shutil.rmtree(os.path.join(root, d))
